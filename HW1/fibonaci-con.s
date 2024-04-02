1f58fd6f05d480ffa292b1dbc7b745c54fb01251
bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>
2024-04-03 00:53:04 +0800

    [upload] add original Cpp file And MIPS Assembley Solving Fibonaci by using assembley
    Signed-off-by: bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>

--- HW1/fibonaci-con.s
+++ HW1/fibonaci-con.s
@@ -1,44 +1,26 @@
     .text
 main:
-    li $s0, 0
-loop:
+    li $t1, 0
+    li $t2, 1
     li $v0, 5
+    li $s1, 32
     syscall
-    beq $v0, 0, exit
-    add $a0 ,$v0, $0
-    jal fibo
-    li $s0, 1
+    add $t0, $v0, $0
+loop:
+    beq $t0, 0, exit
+    add $a0, $t1, $0
+    li $v0, 1
     syscall
+    beq $t0, 1, skip
+    add $a0, $s1, $0
+    li $v0, 11
+    syscall
+skip:
+    add $t3, $t1, $t2
+    add $t1, $t2, $0
+    add $t2, $t3, $0
+    subi $t0, $t0, 1
     j loop
-fibo:
-    # addi $sp, $sp, -8
-    # sw $ra, 4($sp)
-    # sw $a0, 0($sp)
-    # addi $a0, $a0 ,-1
-    # beq $a0 0 L1
-    # jal fibo
-    # add $s0 ,$s0, $v0
-    # addi $a0, $a0 ,-1
-    # beq $a0 0 L1
-    # jal fibo
-    # add $s0 ,$s0, $v0
-    # add $s0 ,$v0, $v1
-    # lw $a0, 0($sp)
-    # lw $ra, 4($sp)
-    # addi $sp, $sp, 8
-    # jr $ra
-    leq $t1, $a0, 1
-    beq $t1, 1, L1
-    
-L1:
-    li $t1,0
-    sw $t1,0($sp)
-    addi $sp, $sp, -4
-    li $t1,1
-    sw $t1,4($sp)
-    # li $t1 0
-    # add $s0 ,$s0, $t1
-    # jr $ra
 exit:
     li $v0 10
     syscall
