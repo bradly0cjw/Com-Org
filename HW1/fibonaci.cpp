c1153ca0e1f64afe290efb28f74633a4060d5ee4
bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>
2024-04-03 01:53:47 +0800

    [fix] fix load function
    Signed-off-by: bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>

--- HW1/fibonaci.cpp
+++ HW1/fibonaci.cpp
@@ -1,16 +1,17 @@
-#include "bits/stdc++.h"
+#include "iostream"
 using namespace std;
 
 int main(){
     int n;
-    cin >> n;
-    int a = 0, b = 1;
-    for(int i = 0; i < n; i++){
-        cout << a << " ";
-        int c = a + b;
-        a = b;
-        b = c;
+    while(cin>>n){
+        int a = 0, b = 1;
+        for(int i = 0; i < n; i++){
+            cout << a << " ";
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        cout << endl;
+        return 0;
     }
-    cout << endl;
-    return 0;
 }
